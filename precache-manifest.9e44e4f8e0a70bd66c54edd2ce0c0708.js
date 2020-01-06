3affb86e27d0dd6a8a89b62290106b36e2f1acb3
Shrikant Singh <Shrikant@cloudthing.com>
2020-01-06 17:03:42 +0530

    Updates

---
+++ precache-manifest.9e44e4f8e0a70bd66c54edd2ce0c0708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e60ca03d6a2541896b298eca13b070b",
+    "url": "/react-app-userprofile/index.html"
+  },
+  {
+    "revision": "2c54b461e57b3bbc9a2c",
+    "url": "/react-app-userprofile/static/css/main.c1036d16.chunk.css"
+  },
+  {
+    "revision": "2b539879263ab0bc9105",
+    "url": "/react-app-userprofile/static/js/2.f1c904bb.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-app-userprofile/static/js/2.f1c904bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c54b461e57b3bbc9a2c",
+    "url": "/react-app-userprofile/static/js/main.1dca3251.chunk.js"
+  },
+  {
+    "revision": "4f7cdc255e554f5582b8",
+    "url": "/react-app-userprofile/static/js/runtime-main.642a1a1e.js"
+  }
+]);
