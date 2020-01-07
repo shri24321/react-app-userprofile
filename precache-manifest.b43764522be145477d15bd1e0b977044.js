684897befcd0d441b9bbd06b551e7b628850f59a
Shrikant Singh <Shrikant@cloudthing.com>
2020-01-07 09:29:32 +0530

    Updates

--- precache-manifest.b43764522be145477d15bd1e0b977044.js
+++ precache-manifest.b43764522be145477d15bd1e0b977044.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5c81bc2c65083605348145e6a8add2a9",
+    "revision": "d1cb3ea2eaae20dec9aa234283c02f59",
     "url": "/react-app-userprofile/index.html"
   },
   {
-    "revision": "25f695eed6b966369d41",
+    "revision": "194cd20e1435995a95ed",
     "url": "/react-app-userprofile/static/css/main.c1036d16.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-app-userprofile/static/js/2.5866b1bf.chunk.js.LICENSE"
   },
   {
-    "revision": "25f695eed6b966369d41",
-    "url": "/react-app-userprofile/static/js/main.b4df405d.chunk.js"
+    "revision": "194cd20e1435995a95ed",
+    "url": "/react-app-userprofile/static/js/main.0b157334.chunk.js"
   },
   {
     "revision": "4f7cdc255e554f5582b8",
